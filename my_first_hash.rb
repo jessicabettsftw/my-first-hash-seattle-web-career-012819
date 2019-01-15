6bc97c7c474ecd14fa8b278bf236a9073a2a0474
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-15 09:51:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,7 +12,7 @@ def shipping_manifest
   the_manifest["whale bone corsets"] = 5
   the_manifest["porcelain vases"] = 2 
   the_manifest["oil paintings"] = 3
-
+  return the_manifest
 end
 
 def retrieval
